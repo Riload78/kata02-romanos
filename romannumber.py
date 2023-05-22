892115c3ba73c0aaf2cc144c92ee8ac34cb013b8
Riload <ricardovlmmarcos@gmail.com>
2023-05-22 23:03:40 +0200

    Funciones romanos

--- romannumber.py
+++ romannumber.py
@@ -1,38 +1,88 @@
 
-simbolos = {
+unidades = {
     1:'I',
     5:'V',
-    10:'X',
-}   
+    10:'X'
+}  
 
-def entero_a_romano(n_int):
-    if n_int in simbolos:  
-        return simbolos[n_int]
-    if n_int < 4:
-        return n_int * simbolos[1]
-    elif n_int == 4 :
-         return simbolos[1] + simbolos[5]
-    elif n_int > 4 and n_int < 9: 
-        multiplicador = n_int - 5
-        return simbolos[5] + multiplicador * simbolos[1]
-    elif n_int == 9 :
-        return simbolos[1] + simbolos[5]
-    elif n_int == 10 :
-        return simbolos[10]
-    elif n_int > 10 :
-        return simbolos[10] + (n_int - 10) * simbolos[1]
-    elif n_int == 14:
-        return simbolos[10] + simbolos[1] + simbolos[5]
-    elif n_int > 14:
-        return simbolos[10] + simbolos[5]
+decenas = {
+    1: 'X',
+    5: 'L',
+    10: 'C'
+}
+
+centenas = {
+    1: 'C',
+    5: 'D',
+    10: 'M'
+}
+
+millares = {
+    1: 'M'
+}
+
+class RomanNumberError(Exception):
+    pass
+
+
+def lista_numero(num):
+    n_mil = num // 1000 * 1000
+    n_cen = (num % 1000) // 100 * 100
+    n_dec = (num % 100) // 10 * 10
+    n_uni = (num % 10)
+    
+    digitos = [n_mil, n_cen, n_dec, n_uni]
+    return digitos
+
+def sacar_clave(num):
+    if num >= 1000 :
+        clave = millares
+        num = num // 1000
+            
+    elif num >= 100 :
+        clave = centenas
+        num //= 100
+    
+    elif num >= 10:
+        clave = decenas
+        num = num // 10
+    else:
+        clave = unidades  
+        
+    return clave, num 
+
+def set_number(digito,clave):
+    resultado = ''
+    if digito < 4:
+        resultado += digito * clave[1] # 2 3
+    elif digito == 4 :
+        resultado += clave[1] + clave[5]
+    elif digito < 9: 
+        multiplicador = digito - 5
+        resultado += clave[5] + multiplicador * clave[1]
+    else :
+        resultado += clave[1] + clave[10]
+        
+    return resultado
     
 
-def convert_num(num):
-    list_num = []
-    str_num = str(num)
-    for n in str_num :
-       list_num.append(n)
-    return list_num
+def entero_a_romano(n_int):
+    if n_int > 4000:
+        raise RomanNumberError('RomanErrror must be less of 4000')
+    
+    digitos = lista_numero(n_int);
+    resultado = ''
+    
+    for digito in digitos :
+        if digito == 0:
+            continue
+        
+        clave, digito = sacar_clave(digito)
+        resultado += set_number(digito, clave)
+  
+    return resultado
+    
+
 
 if __name__ == "__main__":
-    print(entero_a_romano(11))
+    print(entero_a_romano(2345))
--- test_romannumber.py
+++ test_romannumber.py
@@ -1,3 +1,5 @@
+import pytest
+from romannumber import RomanNumberError
 from romannumber import entero_a_romano
 
 
@@ -25,23 +27,38 @@ def test_siete_es_uve_dos_palitos():
 def test_ocho_es_uve_tres_palitos():
     assert entero_a_romano(8) == 'VIII'
     
-def test_nueve_es_palito_uve():
-    assert entero_a_romano(9) == 'IV'
+def test_nueve_es_palito_x():
+    assert entero_a_romano(9) == 'IX'
 
 def test_diez_es_x():
     assert entero_a_romano(10) == 'X'   
     
-def test_once_es_x_palito():
+    
+def test_dieciocho_x_v_trespalito():
+    assert entero_a_romano(20) == 'XX'
+    
+def test_resto_de_decenas():
+    assert entero_a_romano(30) == 'XXX'
+    assert entero_a_romano(40) == 'XL'
+    assert entero_a_romano(50) == 'L'
+    assert entero_a_romano(60) == 'LX'
+    assert entero_a_romano(70) == 'LXX'
+    assert entero_a_romano(80) == 'LXXX'
+    assert entero_a_romano(90) == 'XC'
+    
+def test_once_es_equis_palito():
     assert entero_a_romano(11) == 'XI'
-
-def test_doce_x_palito_palito():
-    assert entero_a_romano(12) == 'XII'
     
-def test_trece_x_palito_palito():
-    assert entero_a_romano(13) == 'XIII'
-
-def test_catorce_x_uve_palito():
-    assert entero_a_romano(14) == 'XIV'
+def test_101_es_C_palito():
+    assert entero_a_romano(101) == 'CI'
     
-def test_quince_v():
-    assert entero_a_romano(15) == 'XV'
+def test_cero_es_nada():
+    assert entero_a_romano(0) == ""
+    
+def test_miles():
+    assert entero_a_romano(2345) == 'MMCCCXLV'
+    assert entero_a_romano(3999) == 'MMMCMXCIX'
+
+def test_4000_es_error():
+    with pytest.raises(RomanNumberError):
+        entero_a_romano(4000)
